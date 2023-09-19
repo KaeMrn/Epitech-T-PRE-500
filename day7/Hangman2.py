969a13160804f0083e26cf6f12d09ea0012242ba
MERAIN Kawther <merain.kawther@gmail.com>
2023-09-19 14:25:00 +0200

    game changes

--- day7/Hangman2.py
+++ day7/Hangman2.py
@@ -1,6 +1,6 @@
 import time
 
-name = input("What's your name?")
+name = input("What's your name? ")
 print("Hello ", name," it's time to play")
 
 time.sleep(1)
@@ -12,26 +12,28 @@ guesses = ''
 turns = 10
 
 while turns > 0:
-    failed = 0
+    guess = input("\nGuess a character: ").lower()
+                      
+    if len(guess) == 1:
+     if guess in word:
+      guesses += guess
+     else:
+       turns -= 1
+       print("wrong")
+       print ("You have", turns, 'more guesses' )
+    if guess == word:
+        print("Victory!")
+        time.sleep(1)
+        print("GAME OVER")
+        break
+    if turns == 0:
+       print("You lose")
+       time.sleep(1)
+       print("GAME OVER")
+       break
+
     for char in word:
         if char in guesses:
             print(char, end="")
         else: 
             print(" _ ", end="")
-            failed += 1
-    if failed == 0:
-        print("Victory!")
-        break
-    guess = input("Guess a character: ")
-    if len(guess) == 1:
-     guesses += guess
-     if guesses not in word:
-       turns -= 1
-       print("wrong")
-       print ("You have", + turns, 'more guesses' )
-    if guess == word:
-        print("Victory!")
-        break
-    if turns == 0:
-       print("You lose")
-       break
